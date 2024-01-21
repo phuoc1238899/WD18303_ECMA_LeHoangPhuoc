d4c734033eb235eb42040258759f12755b8e8b4f
phuoc1238899 <145195528+phuoc1238899@users.noreply.github.com>
2024-01-21 14:50:28 +0700

    Assignment1 - sua

--- admin/main2.js
+++ admin/main2.js
@@ -1,66 +1,38 @@
 fetch("../site/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    let headers = ["Tên Sản Phẩm","Loại", "Hình", "Giá", "Chức năng"];
-    let table = document.getElementById("datatablesSimple");
-    table.innerHTML = generateTable(data.products);
+    .then((response) => response.json())
+    .then((data) => {
+        let table = document.getElementById("datatable");
+        table.innerHTML = generateTable(data.products);
 
-    function generateTable(products) {
-      if (!products || products.length === 0) {
-        return "";
-      }
+        function generateTable(products) {
+            if (!products || products.length === 0) {
+                return "";
+            }
 
-      let tableHTML = `
-        <thead>
-          <tr>
-            ${generateTableHeader(headers)}
-          </tr>
-        </thead>
-        <tbody>
-          ${generateTableRows(products)}
-        </tbody>
+            let rowsHTML = "";
+            products.forEach((product) => {
+                rowsHTML += generateTableRow(product);
+            });
+
+            return rowsHTML;
+        }
+
+        function generateTableRow(product) {
+            return `
+      <tr>
+      <td>${product.name}</td>
+      <td>${product.category}</td>
+      <td><img src="../site/${product.image}" width="50" height="50"></td>
+      <td>${product.price}đ</td>
+      <td>
+    <button class="btn btn-danger" onclick="deleteProduct(${product.id})">
+      Xóa
+    </button>
+    <button class="btn btn-primary" onclick="editProduct(${product.id})">
+      Sửa
+    </button>
+  </td>
+    </tr>
       `;
-      return tableHTML;
-    }
-
-    function generateTableHeader(headerTitles) {
-      if (!headerTitles || headerTitles.length === 0) {
-        return "";
-      }
-      let html = "";
-      headerTitles.forEach((element) => {
-        html += `<th>${element}</th>`;
-      });
-      return html;
-    }
-
-    function generateTableRows(products) {
-      if (!products || products.length === 0) {
-        return "";
-      }
-      let rowsHTML = "";
-      products.forEach((product) => {
-        rowsHTML += generateTableRow(product);
-      });
-      return rowsHTML;
-    }
-
-    function generateTableRow(product) {
-      return `
-        <tr>
-          <td>${product.name}</td>
-          <td>${product.category}</td>
-          <td><img src="../site/${product.image}" width="50" height="50"></td>
-          <td>${product.price}đ</td>
-          <td>
-        <button class="btn btn-danger" onclick="deleteProduct(${product.id})">
-          Xóa
-        </button>
-        <button class="btn btn-primary" onclick="editProduct(${product.id})">
-          Sửa
-        </button>
-      </td>
-        </tr>
-      `;
-    }
-  });
+        }
+    });
