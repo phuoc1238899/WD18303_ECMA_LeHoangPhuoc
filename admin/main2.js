1529dd261c6d7fb29039f59413c27436680a5fbf
phuoc1238899 <145195528+phuoc1238899@users.noreply.github.com>
2024-01-24 16:54:03 +0700

    Assignment1 - dofulldulieuadmin

--- admin/main2.js
+++ admin/main2.js
@@ -1,22 +1,22 @@
 fetch("../site/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    let table = document.getElementById("datatable");
-    table.innerHTML = generateTable(data.products);
+    .then((response) => response.json())
+    .then((data) => {
+        let table = document.getElementById("datatableproduct");
+        table.innerHTML = generateTable(data.products);
 
-    function generateTable(products) {
-      if (!products || products.length === 0) {
-        return "";
-      }
-      let rowsHTML = "";
-      products.forEach((product) => {
-        rowsHTML += generateTableRow(product);
-      });
-      return rowsHTML;
-    }
+        function generateTable(products) {
+            if (!products || products.length === 0) {
+                return "";
+            }
+            let rowsHTML = "";
+            products.forEach((product) => {
+                rowsHTML += generateTableRow(product);
+            });
+            return rowsHTML;
+        }
 
-    function generateTableRow(product) {
-     return ` <tr>
+        function generateTableRow(product) {
+            return ` <tr>
      <table>
   <tr>
     <th>Tên</th>
@@ -40,22 +40,22 @@ fetch("../site/data.json")
     </tr>
     </table>
       `;
-    }
-  });
-  fetch("../site/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    let tableBody = document.querySelector("#categoris2");
-    tableBody.innerHTML = generateTableRows(data.categories);
+        }
+    });
+fetch("../site/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let tableBody = document.querySelector("#datatablecategoris");
+        tableBody.innerHTML = generateTableRows(data.categories);
 
-    function generateTableRows(categories) {
-      if (!categories || categories.length === 0) {
-        return "<tr><td colspan='2'>No categories found</td></tr>";
-      }
+        function generateTableRows(categories) {
+            if (!categories || categories.length === 0) {
+                return "<tr><td colspan='2'>No categories found</td></tr>";
+            }
 
-      let rows = "";
-      categories.forEach((category) => {
-        rows += `
+            let rows = "";
+            categories.forEach((category) => {
+                rows += `
         <table>
   <tr>
     <th>Id</th>
@@ -68,8 +68,66 @@ fetch("../site/data.json")
   <tr>
 </table>
         `;
-      });
+            });
 
-      return rows;
-    }
-  });
+            return rows;
+        }
+    });
+fetch("../site/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let table = document.getElementById("datatableoderss");
+        table.innerHTML = generateTable(data.orders);
+
+        function generateTable(orders) {
+            if (!orders || orders.length === 0) {
+                return "";
+            }
+            let rowsHTML = "";
+            orders.forEach((order) => {
+                rowsHTML += generateTableRow(order);
+            });
+            return rowsHTML;
+        }
+
+        function generateTableRow(order) {
+            return `
+        <tr>
+          <td>${order.customer_name}</td>
+          <td>${order.customer_address}</td>
+          <td>${order.customer_email}</td>
+          <td>${order.customer_phone_number}</td>
+          <td>${order.created_date}</td>
+          <td>${order.status}</td>
+        </tr>
+      `;
+        }
+    });
+fetch("../site/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let orderDetailTable = document.getElementById("datatableoder_detail");
+        orderDetailTable.innerHTML = generateOrderDetailTable(data.order_detail);
+
+        function generateOrderDetailTable(orderDetail) {
+            if (!orderDetail || orderDetail.length === 0) {
+                return "";
+            }
+            let rowsHTML = "";
+            orderDetail.forEach((detail) => {
+                rowsHTML += generateOrderDetailRow(detail);
+            });
+            return rowsHTML;
+        }
+
+        function generateOrderDetailRow(detail) {
+            return `
+        <tr>
+          <td>${detail.order_id}</td>
+          <td>${detail.product_id}</td>
+          <td>${detail.quantity}</td>
+          <td>${detail.unit_price}</td>
+        </tr>
+      `;
+        }
+    });
